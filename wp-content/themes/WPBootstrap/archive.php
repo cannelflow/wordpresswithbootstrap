95727d4f01f973d0c5ea5fafbd7758c5c0dab8d6
cannelflow <mightyrocket@hotmail.com>
2017-01-14 23:02:02 +0000

    Video 6

--- wp-content/themes/WPBootstrap/archive.php
+++ wp-content/themes/WPBootstrap/archive.php
@@ -6,22 +6,29 @@
           
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <div class="blog-post">
-            <h2 class="blog-post-title"><?php the_title() ; ?></h2>
+            <h2 class="blog-post-title"><a href="<?php the_permalink() ; ?>"><?php the_title() ; ?></a></h2>
+            <p class="blog-post-meta">Posted by <?php the_author_posts_link(); ?> on <?php the_time('F j, Y'); ?></p>
             <?php if ( has_post_thumbnail() ) : ?>
                    <div class="blog-post-img">
                         <?php the_post_thumbnail(); ?>
                    </div>
             <?php endif ; ?>       
             
-            <div class="blog-post-content">
-                <?php the_content() ; ?>
+            <div class="blog-post-excerpt">
+                <?php the_excerpt() ; ?>
             </div>
           </div><!-- /.blog-post -->
           <?php endwhile; else : ?>
 	      <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
           <?php endif; ?>
          
-        </div><!-- /.blog-main -->
+          <nav>
+            <ul class="pager">
+              <li><a href="#">Previous</a></li>
+              <li><a href="#">Next</a></li>
+            </ul>
+          </nav>
 
+        </div><!-- /.blog-main -->
 
 <?php get_footer() ; ?>
--- wp-content/themes/WPBootstrap/functions.php
+++ wp-content/themes/WPBootstrap/functions.php
@@ -55,6 +55,43 @@ if( !function_exists('ie_scripts')) {
    	add_action('wp_head', 'ie_scripts');
 } // end if
 
+// Widget Locations
+function wpbootstrap_init_widgets($id){
+  register_sidebar(array(
+    'name'  => 'Sidebar',
+    'id'    => 'sidebar',
+    'before_widget' => '<div class="sidebar-module">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>'
+  ));
+  register_sidebar(array(
+    'name'  => 'Box1',
+    'id'    => 'box1',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+  register_sidebar(array(
+    'name'  => 'Box2',
+    'id'    => 'box2',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+  register_sidebar(array(
+    'name'  => 'Box3',
+    'id'    => 'box3',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+}
+add_action('widgets_init', 'wpbootstrap_init_widgets');
+
 /**
  * Replaces the excerpt "more" text by a link.
  */
